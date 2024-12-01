e568c9b45a45f1aba211d759b1b605d6f3bf4889
Uxugin <git@uxugin.com>
2024-12-01 13:15:21 -0700

    AAAAAAAA

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,49 @@
 use cairo::{Context, Error};
-use glib::clone;
-use gtk4::prelude::*;
-use gtk4::{cairo, glib, DrawingArea};
+use gtk4::{cairo, glib::clone, prelude::*, DrawingArea};
 use std::cell::RefCell;
 use std::rc::Rc;
 pub trait Draggable {
     fn draw(&self, context: &Context, x: f64, y: f64) -> Result<(), Error>;
 }
+struct DraggableSetHolder {
+    draggables: Vec<Rc<RefCell<dyn Draggable>>>,
+}
+impl DraggableSetHolder {
+    fn new() -> Self {
+        Self {
+            draggables: Vec::new(),
+        }
+    }
+    /*fn iter(&self) -> std::slice::Iter<Rc<RefCell<dyn Draggable>>> {
+        self.draggables.iter()
+    }*/
+    fn for_each(&self, mut func: impl FnMut(&dyn Draggable) -> ()) {
+        for i in &self.draggables {
+            func(&*i.borrow());
+        }
+    }
+}
 pub struct DragArea {
-    drawing_area: Rc<DrawingArea>,
-    draggables: Rc<RefCell<Vec<Box<dyn Draggable>>>>,
+    drawing_area: DrawingArea,
+    draggables: Rc<RefCell<DraggableSetHolder>>,
 }
 impl DragArea {
     pub fn new(width: i32, height: i32) -> Self {
-        let draggables = Rc::new(RefCell::new(Vec::<Box<dyn Draggable>>::new()));
-        let drawing_area = Rc::new(
-            DrawingArea::builder()
-                .content_width(width)
-                .content_height(height)
-                .build(),
-        );
+        let draggables = Rc::new(RefCell::new(DraggableSetHolder::new()));
+        let drawing_area = DrawingArea::builder()
+            .content_width(width)
+            .content_height(height)
+            .build();
         drawing_area.set_draw_func(clone!(
             #[strong]
-            drawing_area,
-            #[strong]
             draggables,
-            move |_drawing_area: &DrawingArea, context: &Context, _width: i32, _height: i32| {
-                for i in draggables.borrow().iter().rev() {
-                    i.draw(&context, 0.0, 0.0).unwrap();
-                }
+            move |_drawing_area, context, _width, _height| {
+                /*for i in draggables.borrow().iter().rev() {
+                    i.borrow().draw(&context, 0.0, 0.0).unwrap();
+                }*/
+                draggables.borrow().for_each(|x| {
+                    x.draw(&context, 0.0, 0.0).unwrap();
+                });
             }
         ));
         Self {
