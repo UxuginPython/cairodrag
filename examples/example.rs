67b9e04c0b7d86be14644270080d39b6f31f80f8
Uxugin <git@uxugin.com>
2024-12-02 23:39:33 -0700

    make it a bit more presentable

--- examples/example.rs
+++ examples/example.rs
@@ -3,8 +3,8 @@ use cairodrag::*;
 use gtk4::prelude::*;
 use gtk4::{cairo, glib, Application, ApplicationWindow};
 const APP_ID: &str = "com.uxugin.cairodrag.test";
-struct Thing(f64, f64, f64);
-impl Draggable for Thing {
+struct Square(f64, f64, f64);
+impl Draggable for Square {
     fn draw(&self, context: &Context, x: f64, y: f64) -> Result<(), Error> {
         context.set_source_rgb(self.0, self.1, self.2);
         context.rectangle(x, y, 100.0, 100.0);
@@ -15,10 +15,10 @@ impl Draggable for Thing {
         (0.0, 100.0, 0.0, 100.0)
     }
 }
-struct Circle;
+struct Circle(f64, f64, f64);
 impl Draggable for Circle {
     fn draw(&self, context: &Context, x: f64, y: f64) -> Result<(), Error> {
-        context.set_source_rgb(0.0, 1.0, 0.0);
+        context.set_source_rgb(self.0, self.1, self.2);
         context.arc(x, y, 50.0, 0.0, 6.29);
         context.fill()?;
         Ok(())
@@ -30,16 +30,15 @@ impl Draggable for Circle {
         (x.powi(2) + y.powi(2)).sqrt() <= 50.0
     }
 }
-#[test]
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_ui);
     app.run()
 }
 fn build_ui(app: &Application) {
-    let a = Box::new(Thing(1.0, 0.0, 0.0));
-    let b = Box::new(Thing(0.0, 0.0, 1.0));
-    let c = Box::new(Circle);
+    let a = Box::new(Square(1.0, 0.0, 0.0));
+    let b = Box::new(Square(0.0, 0.0, 1.0));
+    let c = Box::new(Circle(0.0, 1.0, 0.0));
     let mut drag_area = DragArea::new(500, 500);
     drag_area.push_box(a, 100.0, 100.0);
     drag_area.push_box(b, 300.0, 100.0);
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use cairo::{Context, Error};
 use glib::Object;
 use gtk4::{cairo, glib, subclass::prelude::*, DrawingArea};
+use std::cell::RefCell;
+use std::rc::Rc;
 mod imp;
 pub trait Draggable {
     fn draw(&self, context: &Context, x: f64, y: f64) -> Result<(), Error>;
@@ -25,7 +27,20 @@ impl DragArea {
             .build()
     }
     pub fn push_box(&mut self, item: Box<impl Draggable + 'static>, x: f64, y: f64) {
-        let selff = imp::DragArea::from_obj(self);
-        selff.push_box(item, x, y);
+        let self_imp = imp::DragArea::from_obj(self);
+        self_imp.push_box(item, x, y);
+    }
+    pub fn push_rc(&mut self, item: Rc<impl Draggable + 'static>, x: f64, y: f64) {
+        let self_imp = imp::DragArea::from_obj(self);
+        self_imp.push_rc(item, x, y);
+    }
+    pub fn push_rc_ref_cell(
+        &mut self,
+        item: Rc<RefCell<impl Draggable + 'static>>,
+        x: f64,
+        y: f64,
+    ) {
+        let self_imp = imp::DragArea::from_obj(self);
+        self_imp.push_rc_ref_cell(item, x, y);
     }
 }
